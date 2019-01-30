41a7af5d8684641a3aea1eb7e1d9913db374e01b
safwaanep <s.patel@rt-iut.re>
2019-01-30 13:58:51 +0100

    Tests et Cobertura

--- src/test/java/package/AppTest.java
+++ src/test/java/package/AppTest.java
@@ -1,40 +1,17 @@
 package 2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+    @Test
+    public void firstAssertTest()
     {
-        super( testName );
+        assertThat(true).isTrue();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
