3425833e75ec50a84fcaf57caaa9796b48f718a6
safwaanep <s.patel@rt-iut.re>
2019-02-27 23:29:03 +0100

    Correction ProductTest et packages

--- src/test/java/fr/esiea/ProductTest.java
+++ src/test/java/fr/esiea/ProductTest.java
@@ -1,60 +1,42 @@
-import java.util.Arrays;
-import java.util.List;
+package fr.esiea;
 
-import org.assertj.core.api.Assertions;
-//Import
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class ProductTest {
+    
+@Test 
+public void getUntit_Product_Test() {
 
-	
-	@Test
-    public void testEquals () {
-
-        Product apples = new Product("apples", ProductUnit.Kilo);
-        List<Product> product_discount = Arrays.asList(apples);
-        Discount discount = new Discount(product_discount, "Apples Discount", 0.199);
-
-        Product banane = new Product("banane", ProductUnit.Kilo);
-        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
-        Product apples1 = new Product("apples", ProductUnit.Kilo);
-        Product apples2 = new Product("apples", ProductUnit.Each);
-
-        Assertions.assertThat(apples.equals(apples)).isEqualTo(true);
-        Assertions.assertThat(apples.equals(null)).isEqualTo(false);
-
-        Assertions.assertThat(apples.equals(discount)).isEqualTo(false);
-
-        Assertions.assertThat(apples.equals(banane)).isEqualTo(false);
-        Assertions.assertThat(apples.equals(toothbrush)).isEqualTo(false);
-        
-        Assertions.assertThat(apples.equals(apples1)).isEqualTo(true);
-        Assertions.assertThat(apples.equals(apples2)).isEqualTo(false);
+    Product flour_bag = new Product("flour_bag", ProductUnit.Kilo);
+  	Product flour_bag_bis = flour_bag;
+    Product apple1 = new Product("apple", ProductUnit.Each);
+    Product apple2 = new Product("apple", ProductUnit.Each);
+    Product potato_each = new Product("potato", ProductUnit.Each);
+    Product potato_kilo = new Product("potato", ProductUnit.Kilo);
+    ReceiptItem receiptItem_flour_bag = new ReceiptItem(flour_bag, 4, 5, 20);
 
 
-    }
+    // test of getName()
+    assertThat(flour_bag.getName()).isEqualTo("flour_bag");
+
+    // test of getUnit()
+    assertThat(flour_bag.getUnit()).isEqualTo(ProductUnit.Kilo);
+
+    // test of equals()
+    assertThat(flour_bag.equals(flour_bag_bis)).isEqualTo(true);
+    assertThat(flour_bag.equals(receiptItem_flour_bag)).isEqualTo(false);
+    assertThat(flour_bag.equals(null)).isEqualTo(false);
+    assertThat(apple1.equals(potato_each)).isEqualTo(false);
+    assertThat(potato_each.equals(potato_kilo)).isEqualTo(false);
+    assertThat(apple1.equals(apple2)).isEqualTo(true);
+    
+
+    // test of hashCode()
+    assertThat(apple1.hashCode()).isEqualTo(apple2.hashCode());
+    assertThat(apple1.hashCode()).isNotEqualTo(flour_bag.hashCode()); 
 
 }
 
-
-// Test des 10% de remise sur le rix sachant que le prix normal est de 2.49€ par sachet 
-@Test
-    public void TestTenPercentDiscount() {
-
-        SupermarketCatalog catalog = new FakeCatalog();
-        Product rice = new Product("Rice",ProductUnit.Each);
-        catalog.addProduct(rice, 2.49);
-
-        ShoppingCart cart = new ShoppingCart();
-        cart.addItem(rice);
-
-        Teller teller = new Teller(catalog);
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, rice,10.0);
-
-        Receipt receipt = teller.checksOutArticlesFrom(cart);
-
-        double expectedTotalPrice =  2.49 - ((2.49*10)/100);
-        double totalPrice = receipt.getTotalPrice();
-        Assertions.assertThat(totalPrice).isEqualTo(expectedTotalPrice).as("Get ten percent off discount on the rice bag");
-
-    }
+}
